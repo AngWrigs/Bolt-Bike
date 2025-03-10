3196691c05cd0677807b461b6e4049303ebe1cef
AngWrigs <angelarwrigley@gmail.com>
2025-03-10 15:45:30 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/9PNpd20nA-bOfptX/scene.splinecode'); // Replace this URL if needed
